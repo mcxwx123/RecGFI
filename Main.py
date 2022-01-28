a548252f18aa0825b3783e778df7f9d066e46b51
mcxwx <54138388+mcxwx123@users.noreply.github.com>
2022-01-28 23:44:19 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from data.data_preprocess_1 import data_preprocess1
 from data.data_preprocess_2 import data_preprocess2
 from models.RecGFI import recgfi,recgfi_diffthres,recgfi_timesorted,recgfi_crosspro
+from models.wordcloud import drawwordcloud
 import logging
 if __name__=="__main__":
     logging.basicConfig(
@@ -18,4 +19,6 @@ if __name__=="__main__":
     logging.info("Cross-projects results are available at Cross_projects_dataset.csv.")
     recgfi()
     logging.info("Comparision with baselines are available at Timepoint_1_threshold_0.csv and Timepoint_1_threshold_0.csv.")
+    drawwordcloud()
+    logging.info("Wordclouds of keywords are drawn.")
     logging.info("Finish!")
